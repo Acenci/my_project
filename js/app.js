2b5571bba0618b433633c90a6128a6a53c564f34
Acenci <andrew.cenci@gmail.com>
2017-07-16 18:37:09 -0400

    final update to location images, and added media queries

--- js/app.js
+++ js/app.js
@@ -1,17 +1,31 @@
 //Create lightbox for about.png, when about is clicked.
+  var $overlay = $('<div id="overlay"></div>');
+  var $image = $("<img>");
+
+  $overlay.append($image);
+
+  //Add Overlay
+  $("body").append($overlay);
+
   //Capture the click event
   $("#aboutLightbox").click(function(event){
     event.preventDefault();
-    var href = $(this).attr("href");
-    console.log(href);
+    var imageLocation = $(this).attr("href");
+    $image.attr("src", imageLocation);
+
+    $overlay.show();
   });
     //Show Overlay
     //Show about.png
+
+
   //When overlay is clicked hide lightbox
+  $overlay.click(function(){
+    $overlay.hide();
+  });
 
 
-
-//Create lightbox or Overlay for image galleries, jQuery.
+//Create lightbox or Overlay for image galleries, jQuery Watch same videos, "Preform part 4 for caption."
 
 //Create a contact box (Reference from Class Challenge 4)
 // ### Challenge 4
